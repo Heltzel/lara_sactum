e845f72a8be5c9960a76fd524eb8f039f49da9a9
Heltzel <heltzel.service@hotmail.com>
2021-04-01 19:08:55 +0200

    Endpoints  for auth

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,24 @@
 <?php
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Route::resource('products', ProductsController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::resource('products', ProductsController::class);
+Route::get('/products', [ProductsController::class, 'index']);
+Route::get('/products/{id}', [ProductsController::class, 'show']);
 Route::get('products/search/{name}', [ProductsController::class, 'search']);
-// Route::get('/products', [ProductsController::class, 'index']);
-// Route::post('/products', [ProductsController::class, 'store']);
-// Route::get('/products/{id}/show', [ProductsController::class, 'show']);
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductsController::class, 'store']);
+    Route::post('/products/{id}', [ProductsController::class, 'update']);
+    Route::post('/products/{id}', [ProductsController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
